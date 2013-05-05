10754a1e2968abd1d67743c8ef6f4b4dae388014
Ziad Gholmish <ZiadGholmish@gmail.com>
2013-05-05 07:48:27 -0700

    Update MainActivity.java

--- src/com/ziadgholmish/customcamera/MainActivity.java
+++ src/com/ziadgholmish/customcamera/MainActivity.java
@@ -16,12 +16,18 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
 
+//the acivity implements SurfaceHolder.Callback to call when the camera take the pic
 public class MainActivity extends Activity implements SurfaceHolder.Callback {
 
+        //image view to show the image that taken from the camera
 	ImageView iview;
+	//surface holder to deal with the surface view 
 	SurfaceHolder sHolder;
+	//surface holder that i will show the camera view in it
 	SurfaceView sView;
+	//declare camera variable
 	Camera mcam;
+	//button that capture the image 
 	Button btn_capture;
 
 	@Override
@@ -29,37 +35,50 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		
+		//get the image view from the layout
 		iview = (ImageView) findViewById(R.id.imageView1);
+		//get the surface view from the layout
 		sView = (SurfaceView) findViewById(R.id.surfaceView1);
+		//get the button
 		btn_capture = (Button) findViewById(R.id.button1);
+		//get the surface holder from the surface view and assign it to the sholder
 		sHolder = sView.getHolder();
-
+                //add listner to the sholder
 		sHolder.addCallback(this);
 		// deprecated setting, but required on Android versions prior to 3.0
 
+               
 		sHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 
 		 final PictureCallback mpic = new PictureCallback() {
 
+
+
+                          //this method called back when the camera take pic 
 			@Override
 			public void onPictureTaken(byte[] arg0, Camera arg1) {
 
+                                      //convert the byte array to bitmap
 				Bitmap bit = BitmapFactory
 						.decodeByteArray(arg0, 0, arg0.length);
+						//set the bit mab to the image view
 				iview.setImageBitmap(bit);
 
 			}
 
 		};
 
+           
 		sHolder.addCallback(this);
+		
+		
+		//set the on clik actionlistner to the button
 		btn_capture.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 
-				
+				//take a pic
 				mcam.takePicture(null, null, mpic);
 
 			}
@@ -74,6 +93,10 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
 		return true;
 	}
 
+
+
+
+        
 	@Override
 	public void surfaceChanged(SurfaceHolder holder, int format, int width,
 			int height) {
@@ -81,6 +104,7 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
 		if (mcam != null) {
 
 			try {
+				
 				mcam.setPreviewDisplay(sHolder);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
@@ -116,6 +140,7 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
 	@Override
 	public void surfaceDestroyed(SurfaceHolder holder) {
 
+                 //release camera
 		mcam.release();
 
 	}
